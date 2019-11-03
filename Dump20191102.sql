ceda7471418818fd170c1e803d3caf5bcc59ee45
20191025 <57300134+20191025@users.noreply.github.com>
2019-11-02 22:08:15 -0500

    Update Dump20191102.sql

--- Dump20191102.sql
+++ Dump20191102.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `odin` /*!40100 DEFAULT CHARACTER SET latin1 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `odin`;
 -- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
 --
 -- Host: localhost    Database: odin
@@ -114,7 +116,9 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-11-02 21:28:29
+-- Dump completed on 2019-11-02 22:06:37
+CREATE DATABASE  IF NOT EXISTS `thor` /*!40100 DEFAULT CHARACTER SET latin1 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `thor`;
 -- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
 --
 -- Host: localhost    Database: thor
@@ -635,4 +639,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-11-02 21:28:29
+-- Dump completed on 2019-11-02 22:06:37
